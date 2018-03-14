a82a5bf1d33989d5d2a5b2d60925da40ed516d27
aagiorgitis <angios90@gmail.com>
2018-03-14 13:58:55 +0200

    please specify commit message

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import Timer from './Timer.js';
 import Names from './names.js';
 import Game from './ttt.js';
 
-class AppState extends React.Component {
+class App extends React.Component {
 	
 	render() {
 		return (
 			<div>
 				<DevTools /> 
-				<h3> some react-mobx experiments - <a href="https://github.com/Agios90/myapp">github</a> </h3>
+				<h3> some mobx-react experiments - <a href="https://github.com/Agios90/myapp">github</a> </h3>
 				<span>_______________________________________________</span> 
 				<br />
 				<br />
@@ -42,7 +42,7 @@ class AppState extends React.Component {
 }
 
 render (
-	<AppState />,
+	<App />,
 	document.getElementById("root")
 );
 	
